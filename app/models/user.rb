71140e34de576beaa48224bcf4e55706e4b662b4
Nathan <nathanrmsmith@gmail.com>
2024-02-21 17:13:50 -0500

    Add user model tests & authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || password.present? }
+
+  def self.authenticate_with_credentials(email, password)
+    normalized_email = email.downcase.strip
+    user = User.find_by(email: normalized_email)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
